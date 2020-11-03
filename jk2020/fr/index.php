03dbb3b497fa9ba10c175c1eccb7dd72e8f1c107
DESKTOP-AH89599\mariu <baumann.marius@gmail.com>
2020-11-04 00:15:44 +0100

    Neue anordnung Startseite

--- jk2020/fr/index.php
+++ jk2020/fr/index.php
@@ -3,7 +3,9 @@
 
 <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
 <script src="https://kit.fontawesome.com/39876f8fd5.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" type="text/css" href="https://www.emk-office.ch/resources/CSS/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="https://www.emk-office.ch/resources/theme.css">
+
 <title>EMK-Office</title>
 
 <meta name="description" content="Website der EMK Zentralverwaltung" />
@@ -13,62 +15,79 @@
 
 
 
-<div id="container">
+<div class="container">
 	<div class="icon-line"></div>
 
 	<a href="#"><div class="icon-logo-container"><img class="icon-logo" src="../../2716-991-max.png"></div></a>
-	<a href=".."><div class="button">Für Deutsch klicken Sie hier</div></a>
+	<a href=".."><div class="btn btn-primary">Für Deutsch klicken Sie hier</div></a>
+	<hr>
 	<h2>Page d’accueil Conférence annuelle 2020</h2>
 	<p>Cette année, la session de la partie Suisse et la Conférence annuelle se tiendront majoritairement en ligne. Cette page a pour but de vous aider dans la préparation de ces conférences. Merci de bien vouloir prendre connaissance de l'introduction et des différentes étapes.</p> 
-
+	<hr>
 	<h4>Participer en 4 étapes</h4>
 
 	<p>Dans les prochaines semaines, vous serez initiés à la participation en ligne grâce aux 4 étapes suivantes, destinées à votre préparation. L’étape 1 consistera à vous inscrire, l’étape 2 vous permettra de configurer votre ordinateur pour les sessions. L’étape 3 vous aidera à tester l’installation et voir si tout fonctionne correctement puis l’étape 4 donnera accès à la participation lors des jours de conférence. Cliquez sur les différentes étapes pour en savoir plus.</p> 
-	<div class="kacheln">
-		<div class="align">
-			<a href="anmelden.php">
-				<div class="element">
-					<p class="T2">1. Inscription</p>
-					<i class="front-icon fas fa-file-signature"></i><br/>
-					<p>Inscrivez-vous ici pour participer.<br>&nbsp;</p>
-				</div>
-			</a>
+
+	<div class="row">
+		<div class="col-4">
 			<a href="anleitung.php">
-				<div class="element">
-					<p class="T2">2. Configuration du logiciel</p>
-					<i class="front-icon fas fa-download"></i><br/>
-					<p>Une démonstration vous sera présentée ici pour configurer le programme informatique requis. </p>
+				<div class="kachel">
+					<h5>Étappe - 2 Configuration du logiciel</h5>
+					<i class="front-icon fas fa-download"></i>
+					<p>Une démonstration vous sera présentée ici pour configurer le programme informatique requis.</p>
 				</div>
 			</a>
 		</div>
-		<div class="align">
-			<a href="testsession.php">
-				<div class="element">
-					<p class="T2">Étape 3 – Utilisation du logiciel et participation à une réunion de test</p>
+		<div class="col-4">
+		<a href="testsession.php">
+				<div class="kachel">
+					<h5>Étape 3 – Utilisation du logiciel et participation à une réunion de test</h5>
 					<i class="front-icon fas fa-laptop"></i><br/>
 					<p>Découvrez ici comment utiliser le logiciel et comment participer à une réunion de test. <br/><br/></p>
 				</div>
 			</a>
+		</div>
+		<div class="col-4">
 			<a href="session.php">
-				<div class="element">
-					<p class="T2">Étape 4 – Participer à la Conférence annuelle<br/><br/></p>
+				<div class="kachel">
+					<h5>Étape 4 – Participer à la Conférence annuelle</h5>
 					<i class="front-icon fas fa-laptop"></i><br/>
 					<p>Vous trouverez ici les liens d’accès pour participer à la session de la partie Suisse et à la Conférence annuelle, aux jours respectifs.</p>
-				</div>
+				</div>	
 			</a>
 		</div>
 	</div>
+<hr>
+	<h4>Informations complémentaires</h4>
+	<p>Vous trouverez ici de plus amples informations sur la conférence annuelle.</p> 
 
-		<h4>Informations complémentaires</h4>
-		<p>Vous trouverez ici de plus amples informations sur la conférence annuelle.</p> 
+	<div class="row">
+		<div class="col-6">
+		<a href="https://nx5186.your-storageshare.de/f/178531" target="_blank">
+			<div class="kachel">
+					<h5>Documents CA 2020 </h5>
+					<i class="front-icon fas fa-file"></i><br/>
+					<p>Hier finden Sie die wichtisten Dokumennte in der aktuellsten Fassung.</p>
+				
+			</div>	
+		</div>
+		</a>
+		<div class="col-3">
+			<a href="anmelden.php">
+				<div class="kachel">
+					<h5>Éttape - 1 Inscription</h5>
+					<i class="front-icon fas fa-file-signature"></i>
+					<p>Vous trouverez ici les documents les plus importants dans la dernière version.</p>
+				</div>
+			</a>	
+		</div>
+	</div>
+
 	<div class="kacheln">	
 		<div class="align">
-			
-			<a href="https://nx5186.your-storageshare.de/f/178531" target="_blank">
+			<a href="https://nx5186.your-storageshare.de/f/201077" target="_blank">
 				<div class="element">
-					<p class="T2">Documents CA 2020</p>
-					<i class="front-icon fas fa-file"></i><br/>
-					<p>Vous trouverez ici les documents les plus importants dans la dernière version.</p>
+					
 				</div>
 			</a>
 			<!--
@@ -76,9 +95,10 @@
 				<div class="element">
 					<p class="T2">Tagesprogramm Jährliche Konferenz</p>
 					<i class="front-icon far fa-clock"></i><br/>
-					<p>Sehen Sie den aktuellen Tagesablauf an. Kurzfristige Änderungen werden hier berücksichtig.</p>
+					<p>Sehen Sie sich den aktuellen Tagesablauf an. Kurzfristige Änderungen werden hier berücksichtigt.</p>
 				</div>
-			</a>-->
+			</a>
+			-->
 		</div>
 	</div>
 </div>
