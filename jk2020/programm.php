3d5cbe91eb8dd59d2faa578c9ba0ab51c3f727a8
DESKTOP-AH89599\mariu <baumann.marius@gmail.com>
2020-11-05 01:37:33 +0100

    Programm-Entwurf

---
+++ jk2020/programm.php
@@ -0,0 +1,382 @@
+<html>
+<head>
+
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
+<script src="https://kit.fontawesome.com/39876f8fd5.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" type="text/css" href="https://www.emk-office.ch/resources/CSS/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="https://www.emk-office.ch/resources/theme.css">
+
+<title>EMK-Office</title>
+
+<meta name="description" content="Website der EMK Zentralverwaltung" />
+</head>
+
+<body>
+
+
+
+<div class="container">
+	<div class="icon-line"></div>
+
+	<a href="#"><div class="icon-logo-container"><img class="icon-logo" src="../2716-991-max.png"></div></a>
+	<a href="fr"><div class="btn btn-primary">Pour le français, cliquez ici</div></a>
+	<hr>
+	<h2>Tagung Teil Schweiz</h2>
+	<br/>
+	 
+	<div class="row outer-row">
+		<div class="col-12">
+			<div class="kachel-pgm">
+				<h5><span class="badge badge-secondary">12:00</span> Zoom Offen zum Einwählen</h5>
+				<hr>
+				<a href="fr" class="btn btn-primary">In Zoom-Sitzung einwählen</a>
+				<p></p>
+			</div>
+		</div>
+	</div>
+	<div class="row outer-row">
+		<div class="col-12">
+			<div class="kachel-pgm">
+				<h5><span class="badge badge-primary">13:00</span> Begrüssung</h5>
+			</div>
+		</div>
+	</div>
+	<div class="row outer-row">
+		<div class="col-12">
+			<div class="kachel-pgm"><span class="badge badge-pill badge-secondary float-right">Gruppenarbeit</span>
+				<h5><span class="badge badge-primary">13:02</span> Andacht</h5>
+			</div>
+		</div>
+	</div>
+	<div class="row outer-row">
+		<div class="col-12">
+			<div class="kachel-pgm">
+				<h5><span class="badge badge-primary">13:30</span> Konstituierung</h5>
+				<hr>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right">Zoom-Wahl</span>
+							<h6><span class="badge badge-primary">ff</span> Wahl Helfer</h6>
+							<hr>
+							<a href="fr" class="btn btn-primary"><i class="fas fa-file-pdf"></i> Wahlliste als PDF herunterladen</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row outer-row">
+		<div class="col-12">
+			<div class="kachel-pgm">
+				<h5><span class="badge badge-primary">13:35</span> VO-Bericht Teil 1</h5>
+				<hr>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right"></span>
+							<h6><span class="badge badge-primary">13:35</span> VO-Bericht Kap 1 Jahreszyklus</h6>
+						</div>
+					</div>
+				</div>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right"></span>
+							<h6><span class="badge badge-primary">13:40</span> Hinführung zum Thema Solidarität</h6>
+						</div>
+					</div>
+				</div>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right"></span>
+							<h6><span class="badge badge-primary">13:45</span> 5 Statements zu den Thesen</h6>
+						</div>
+					</div>
+				</div>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-secondary float-right">Gruppenarbeit</span>
+							<h6><span class="badge badge-primary">14:00</span> Disskussion der fünf Thesen</h6>
+							<hr>
+							<a href="fr" class="btn btn-primary"><i class="fas fa-file-pdf"></i> Thesen als PDF herunterladen</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row outer-row">
+		<div class="col-12">
+			<div class="kachel-pgm">
+				<h5><span class="badge badge-warning">14:30</span> Pause <i class="fas fa-mug-hot float-right"></i> </h5>
+				<p>Bitte die Zoom-Sitzung geöffnet lassen und nicht verlassen!</p>
+				<p>Rückmeldugnen aus den Disskusionsgruppen werden währen der Pause eingeblendet</p>
+			</div>
+		</div>
+	</div>
+	<div class="row outer-row">
+		<div class="col-12">
+			<div class="kachel-pgm">
+				<h5><span class="badge badge-primary">14:45</span> VO-Bericht Teil 2</h5>
+				<hr>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right"><i class="fas fa-hand-paper"></i></span>
+							<h6><span class="badge badge-primary">14:45</span> Austausch über das Feedback der Gruppen</h6>
+							<hr>
+							<a href="fr" class="btn btn-primary"><i class="fas fa-file-pdf"></i> Rückumeldungen der Gruppen als PDF herunterladen</a>
+						</div>
+					</div>
+				</div>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right"><i class="fas fa-hand-paper"></i></span>
+							<h6><span class="badge badge-primary">15:10</span> Konferieren zum Antrag 1</h6>
+							<hr>
+							<a href="fr" class="btn btn-primary"><i class="fas fa-file-pdf"></i> Antrag 1 als PDF herunterladen</a>
+						</div>
+					</div>
+				</div>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right">Zoom-Wahl</span>
+							<h6><span class="badge badge-primary">15:25</span> Abstimmung Antrag 1</h6>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row outer-row">
+		<div class="col-12">
+			<div class="kachel-pgm">
+				<h5><span class="badge badge-warning">15:30</span> Pause <i class="fas fa-mug-hot float-right"></i> </h5>
+				<p>Bitte die Zoom-Sitzung geöffnet lassen und nicht verlassen!</p>
+			</div>
+		</div>
+	</div>
+	<div class="row outer-row">
+		<div class="col-12">
+			<div class="kachel-pgm">
+				<h5><span class="badge badge-primary">14:45</span> VO-Bericht Teil 3</h5>
+				<hr>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right"></span>
+							<h6><span class="badge badge-primary">14:45</span> Einführung in Antrag 2 und Abänderungsantrag</h6>
+							<hr>
+							<a href="fr" class="btn btn-primary"><i class="fas fa-file-pdf"></i> Antrag 2 als PDF herunterladen</a>
+							<a href="fr" class="btn btn-primary"><i class="fas fa-file-pdf"></i> Abänderungsantrag als PDF herunterladen</a>
+						</div>
+					</div>
+				</div>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right"></span>
+							<h6><span class="badge badge-primary">ff</span> Vorstellung Antrag 2</h6>
+						</div>
+					</div>
+				</div>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right"></span>
+							<h6><span class="badge badge-primary">ff</span> Vorstellung Abänderungsantrag</h6>
+						</div>
+					</div>
+				</div>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right"><i class="fas fa-hand-paper"></i></span>
+							<h6><span class="badge badge-primary">ff</span> Debatte Abänderungsantrag</h6>
+						</div>
+					</div>
+				</div>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right"></span>
+							<h6><span class="badge badge-primary">ff</span> Votum Andrea Brunner und Roland Affolter</h6>
+						</div>
+					</div>
+				</div>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right">Zoom-Wahl</span>
+							<h6><span class="badge badge-primary">ff</span> Abstimmung Abänderungsantrag</h6>
+						</div>
+					</div>
+				</div>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right"><i class="fas fa-hand-paper"></i></span>
+							<h6><span class="badge badge-primary">ff</span> Debatte Antrag 2</h6>
+						</div>
+					</div>
+				</div>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right"></span>
+							<h6><span class="badge badge-primary">ff</span> Votum Roland Affolter</h6>
+						</div>
+					</div>
+				</div>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right">Zoom-Wahl</span>
+							<h6><span class="badge badge-primary">ff</span> Abstimmung Antrag 2</h6>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row outer-row">
+		<div class="col-12">
+			<div class="kachel-pgm">
+				<h5><span class="badge badge-warning">16:20</span> Pause <i class="fas fa-mug-hot float-right"></i> </h5>
+				<p>Bitte die Zoom-Sitzung geöffnet lassen und nicht verlassen!</p>
+			</div>
+		</div>
+	</div>
+	<div class="row outer-row">
+		<div class="col-12">
+			<div class="kachel-pgm">
+				<h5><span class="badge badge-primary">16:30</span> VO-Bericht Teil 4</h5>
+				<hr>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right"></span>
+							<h6><span class="badge badge-primary">16:30</span> VO-Bericht Kap 2-5</h6>
+						</div>
+					</div>
+				</div>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right"></span>
+							<h6><span class="badge badge-primary">16:40</span> VO-Bericht Teil ZD</h6>
+						</div>
+					</div>
+				</div>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right"></span>
+							<h6><span class="badge badge-primary">16:50</span> ZV: Jahresrechnung</h6>
+						</div>
+					</div>
+				</div>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right"><i class="fas fa-hand-paper"></i></span>
+							<h6><span class="badge badge-primary">16:50</span> Info-Abstimmungen</h6>
+						</div>
+					</div>
+				</div>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right">Zoom-Wahl</span>
+							<h6><span class="badge badge-primary">16:52</span> Annahme Bericht</h6>
+						</div>
+					</div>
+				</div>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right"></span>
+							<h6><span class="badge badge-primary">16:55</span> Bericht Kommision 11</h6>
+						</div>
+					</div>
+				</div>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right"></span>
+							<h6><span class="badge badge-primary">16:58</span> Info Abstimmungen</h6>
+						</div>
+					</div>
+				</div>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right">Zoom-Wahl</span>
+							<h6><span class="badge badge-primary">17:00</span> Annahme Bericht Kommision 11</h6>
+						</div>
+					</div>
+				</div>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right">Electionbuddy-Wahl</span>
+							<h6><span class="badge badge-primary">17:03</span> Kummulierte Wahlen/Abstimmungen </h6>
+							<hr>
+							<p>Wahl Revisoren, Jahresrechnung, Wahlanträge Kommission 11</p>
+							<p>Bitte prüfen Sie ihr E-Mail-Postfach für die Wahlunterlagen</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row outer-row">
+		<div class="col-12">
+			<div class="kachel-pgm">
+				<h5><span class="badge badge-warning">17:10</span> Pause <i class="fas fa-mug-hot float-right"></i> </h5>
+				<p>Bitte die Zoom-Sitzung geöffnet lassen und nicht verlassen!</p>
+				<p>Wahl-System (Electionbuddy) schliesst um 17:15</p>
+			</div>
+		</div>
+	</div>
+	<div class="row outer-row">
+		<div class="col-12">
+			<div class="kachel-pgm">
+				<h5><span class="badge badge-primary">17:20</span> VO-Bericht Teil 5</h5>
+				<hr>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right"></span>
+							<h6><span class="badge badge-primary">17:20</span> Präventionskonzept VO-Bericht Kap 6</h6>
+						</div>
+					</div>
+				</div>
+				<div class="row outer-row">
+					<div class="col-12">
+						<div class="kachel-pgm"><span class="badge badge-pill badge-info float-right"><i class="fas fa-hand-paper"></i></span>
+							<h6><span class="badge badge-primary">17:30</span> Fragen der Teilnerhmer/innen</h6>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row outer-row">
+		<div class="col-12">
+			<div class="kachel-pgm">
+				<h5><span class="badge badge-primary">17:40</span> Wahlresultate</h5>
+			</div>
+		</div>
+	</div>
+	<div class="row outer-row">
+		<div class="col-12">
+			<div class="kachel-pgm">
+				<h5><span class="badge badge-primary">17:55</span> Einleitung Abschluss der Tagung</h5>
+			</div>
+		</div>
+	</div>
+	<div class="row outer-row">
+		<div class="col-12">
+			<div class="kachel-pgm">
+				<h5><span class="badge badge-primary">18:00</span> Gottesdienst</h5>
+			</div>
+		</div>
+	</div>
+	<div class="row outer-row">
+		<div class="col-12">
+			<div class="kachel-pgm">
+				<h5><span class="badge badge-warning">18:30</span> Feierabend <i class="fas fa-mug-hot float-right"></i> </h5>
+				<p>Nun können Sie die Zoom-Sitzung verlassen.</p>
+			</div>
+		</div>
+	</div>
+	
+<hr>
+	
+</div>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
+</body>
+</html>
